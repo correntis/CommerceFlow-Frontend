06cf193d26bfab0558fad6d07fabcc4cebec926f
Максим Русецкий <maksim.rusetckiy05@mail.ru>
2024-09-08 19:35:43 +0300

    Add AuthService

---
+++ src/app/auth/services/auth.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { env } from '../../../env/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private apiUrl: string = env.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  register(username: string, password: string, email: string) : Observable<any> {
+    return this.http.post(
+      this.apiUrl + "auth/register", 
+      {
+        name: username,
+        password: password,
+        email: email
+      }
+    )
+  }
+}
